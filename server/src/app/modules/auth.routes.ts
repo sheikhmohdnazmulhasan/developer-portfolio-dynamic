8f07e13dafa6d4119a871f32e7fadb39d008b856
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-25 18:57:18 +0600

    feat(auth): validated auth payload with zod

--- server/src/app/modules/auth.routes.ts
+++ server/src/app/modules/auth.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { AuthControllers } from "./auth.controllers";
+import ValidationRequest from "../middlewares/zod-validation";
+import { AuthValidations } from "./auth.validation";
 
 const router: Router = Router();
 
-router.post("/login", AuthControllers.login);
+router.post(
+  "/login",
+  ValidationRequest(AuthValidations.loginValidationSchema),
+  AuthControllers.login
+);
 
 export const AuthRoutes = router;
