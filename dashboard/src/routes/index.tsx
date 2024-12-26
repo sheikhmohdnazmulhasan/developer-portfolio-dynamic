dcd878b3f229294556e368797969875dd5a366da
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-27 00:25:00 +0600

    feat(auth): Router secured. authenticated user cannot be viewed login in Page and unauthenticated user cannot be navigate main page.

--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -1,16 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/auth/login";
 import DashboardRoot from "../pages/dashboard";
+import Protection from "../middleware/protection";
+import AntiLoginProtection from "../middleware/anti-login-protection";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <DashboardRoot />,
+    element: (
+      <Protection>
+        <DashboardRoot />
+      </Protection>
+    ),
   },
 
   {
     path: "/auth/login",
-    element: <Login />,
+    element: (
+      <AntiLoginProtection>
+        <Login />
+      </AntiLoginProtection>
+    ),
   },
 ]);
 
