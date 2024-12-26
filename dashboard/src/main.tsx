994671db7345bcd1db117428b92b6b462671c026
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-26 20:10:56 +0600

    setup redux-persist

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -4,12 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={false} persistor={persistor}></PersistGate>
       <RouterProvider router={router} />
+      <Toaster richColors position="bottom-right" />
     </Provider>
   </React.StrictMode>
 );
