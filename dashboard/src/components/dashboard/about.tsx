ed3dafe4148b06cc91a194f2600479fe696e987f
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-27 12:41:09 +0600

    about done

--- dashboard/src/components/dashboard/about.tsx
+++ dashboard/src/components/dashboard/about.tsx
@@ -1,76 +1,109 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Input, Button } from "antd";
 import RichTextEditor from "../../utils/rich-text-editor";
-import { useFetchAboutQuery } from "../../redux/features/about/about.api";
+import {
+  useEditAboutMutation,
+  useFetchAboutQuery,
+} from "../../redux/features/about/about.api";
 import Loading from "../loading";
+import { toast } from "sonner";
 
 interface ProfileFormValues {
   name: string;
   bio: string;
   designation: string;
-  description: string;
 }
 
 const About: React.FC = () => {
+  const { data: about, isLoading } = useFetchAboutQuery(undefined);
+  const [updateAbout] = useEditAboutMutation();
   const [form] = Form.useForm<ProfileFormValues>();
   const [richText, setRichText] = useState<string>("");
 
-  const { data, isLoading } = useFetchAboutQuery(undefined);
+  useEffect(() => {
+    if (about) {
+      form.setFieldsValue({
+        name: about.name,
+        bio: about.bio,
+        designation: about.designation,
+      });
+    }
+  }, [about, form]);
 
-  const onFinish = (values: ProfileFormValues) => {
+  const onFinish = async (values: ProfileFormValues) => {
     const filteredValues = Object.fromEntries(
       Object.entries(values).filter(([, value]) => value !== undefined)
     );
+
+    try {
+      const response = await updateAbout({
+        ...filteredValues,
+        description: richText,
+      }).unwrap();
+      if (response.success) {
+        toast.success("About successfully updated");
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    }
   };
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <>
-      {isLoading && <Loading />}
-      <Form
-        form={form}
-        name="profile"
-        onFinish={onFinish}
-        layout="vertical"
-        style={{ maxWidth: 600, margin: "0 auto" }}
+    <Form
+      form={form}
+      name="profile"
+      onFinish={onFinish}
+      layout="vertical"
+      initialValues={{
+        name: about?.name || "",
+        bio: about?.bio || "",
+        designation: about?.designation || "",
+      }}
+      style={{ maxWidth: 600, margin: "0 auto" }}
+    >
+      <Form.Item
+        name="name"
+        label="Name"
+        rules={[{ required: true, message: "Please input your name!" }]}
       >
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ message: "Please input your name!" }]}
-        >
-          <Input defaultValue="Sheikh Mohammad Nazmul H." disabled={true} />
-        </Form.Item>
+        <Input disabled />
+      </Form.Item>
 
-        <Form.Item
-          name="bio"
-          label="Bio"
-          rules={[{ required: true, message: "Please input your bio!" }]}
-        >
-          <Input.TextArea rows={3} />
-        </Form.Item>
+      <Form.Item
+        name="bio"
+        label="Bio"
+        rules={[{ required: true, message: "Please input your bio!" }]}
+      >
+        <Input.TextArea rows={3} />
+      </Form.Item>
 
-        <Form.Item
-          name="designation"
-          label="Designation"
-          rules={[
-            { required: true, message: "Please input your designation!" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <RichTextEditor
-          richText={richText}
-          setRichText={setRichText}
-          rows={10}
-        />
+      <Form.Item
+        name="designation"
+        label="Designation"
+        rules={[{ required: true, message: "Please input your designation!" }]}
+      >
+        <Input />
+      </Form.Item>
 
-        <Form.Item className="pt-10">
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
+      <RichTextEditor
+        richText={richText}
+        setRichText={setRichText}
+        defaultRichText={about?.description}
+        rows={10}
+      />
+
+      <Form.Item className="pt-10">
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
